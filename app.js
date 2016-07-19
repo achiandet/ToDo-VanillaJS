d1b70560b3055e13420d20770228e09d94b734bd
Aaron Chiandet <achiandet@gmail.com>
2016-07-18 21:54:47 -0700

    ToDo App v.1

--- app.js
+++ app.js
@@ -7,4 +7,12 @@ It should have a way to change a todo
 It should have a way to delete a todo
 */
 
-var todos = ['item 1', 'item 2', 'item 3'];
+var todos = [' item 1', ' item 2', ' item 3'];
+    // adds item to the todo array
+    todos.push(' item 4', ' item 5', 'item deleted');
+    // allows you to edit the array
+    todos[1] = " item 2 changed"
+    // allows you to delete an item from the array
+    todos.splice(5, 1);
+
+console.log("My todos are" + todos + ".");
